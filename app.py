7074f2648d7b250c84ba117e5de382fa47427031
retiredtotuscany <retiredtotuscany@gmail.com>
2022-04-03 09:04:58 +0200

    Flask

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, redirect, url_for, render_template, url_for
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from datetime import datetime
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+class todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default = 0)
+    date_created = db.Column(db.DateTime, default = datetime)
+    def __repr__(self):
+        return'<Task %r>' % self.id
 @app.route('/')
 def index():
     return render_template('index.html')
